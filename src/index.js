559176406cf753fffd2c7e8c4a04fd45ceff5fe1
Nick <Bauer_Nicholas@bah.com>
2019-10-23 01:28:59 -0400

    Finished Basic Functionalities, Added Reset Button

--- src/index.js
+++ src/index.js
@@ -22,6 +22,9 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       squares: squares,
@@ -34,7 +37,21 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } 
+    /*else if(count > 9){
+      status = "It's A Tie!"
+    }*/
+    else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+  }
+
+  function refreshPage() {
+    window.location.reload(false);
+  }
 
     return (
       <div>
@@ -53,6 +70,9 @@ class Board extends React.Component {
           {this.renderSquare(6)}
           {this.renderSquare(7)}
           {this.renderSquare(8)}
+        </div>
+        <div>
+          <button onClick={refreshPage}>Reset</button>
         </div>
       </div>
     );
@@ -73,6 +93,26 @@ class Game extends React.Component {
       </div>
     );
   }
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 // ========================================
